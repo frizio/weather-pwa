f3d996b4f3e729769a593c1c7dcd9f25f6bff0c2
Maurizio La Rocca <mrz.larocca@gmail.com>
2019-04-05 11:53:20 +0200

    Solving potencial Race condition network-cache

--- scripts/app.js
+++ scripts/app.js
@@ -161,15 +161,20 @@ var injectedForecast = {
       caches.match(url).then(function(response) {
         if (response) {
           response.json().then(function(json) {
-            json.key = key;
-            json.label = label;
-            app.updateForecastCard(json);
+            // Onlu update if the XHR is still pending, 
+            // otherwise the XHR  has already returned and provided the last data
+            if (app.hasRequestPending) {
+              json.key = key;
+              json.label = label;
+              app.updateForecastCard(json);
+            }
           });
         }
       });
     }
     // Type and make an AJAX request (over the NETWORK)
     // Make the XHR to get the data, then update the card
+    app.hasRequestPending = true;
     var request = new XMLHttpRequest();
     request.onreadystatechange = function() {
       if (request.readyState === XMLHttpRequest.DONE) {
@@ -177,6 +182,7 @@ var injectedForecast = {
           var response = JSON.parse(request.response);
           response.key = key;
           response.label = label;
+          app.hasRequestPending = false;
           app.updateForecastCard(response);
         }
       }
